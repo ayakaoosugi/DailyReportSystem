d7d5fa5142160e7c9ab272bc58bcfd07f7c337d8
ayakaoosugi <tantaran924@gmail.com>
2023-03-18 19:54:15 +0900

    Lesson 35Chapter 4.5コミット

---
+++ src/main/java/com/techacademy/entity/Authentication.java
@@ -0,0 +1,44 @@
+package com.techacademy.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "authentication")
+public class Authentication {
+
+
+    /** 主キー。自動生成 */
+    @Id
+    @Column(length = 20, nullable = false)
+    private String code;
+
+    /** パスワード。225桁。null不許可 */
+    @Column(length = 225, nullable = false)
+    private String password;
+
+    /** 権限 */
+    private static enum Role{
+    一般,管理者
+    }
+    @Column(length = 10,nullable=false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    
+
+    /** 従業員のテーブルID */
+    @OneToOne
+    @JoinColumn(name="employee_id", referencedColumnName="id",nullable = false)
+    private Employee employee;
+}
+    
+    
